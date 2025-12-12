dcb32414534d5d33236caa233fe74beb5323dc7b
perezmichael <hi@mikeperezdigital.com>
2025-12-11 23:41:37 -0800

    Fix: Add missing styles and remove old routes

--- src/app/mx-guide/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="h-screen w-full bg-neutral-900 text-white overflow-hidden">
-            {children}
-        </div>
-    );
-}
